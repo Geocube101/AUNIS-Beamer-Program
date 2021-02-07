217b5b2a2292a88320587827bfc2acab9960393f
Geocube101 <40618095+Geocube101@users.noreply.github.com>
2021-02-06 18:26:20 -0600

    Update updater.lua

--- updater.lua
+++ updater.lua
@@ -6,28 +6,39 @@ local kvpreader = require('KeyValuePairFileReader')
 local vinfofile = kvpreader:open(shell.getWorkingDirectory()..'/beamer/data/versioninfo')
 local vinfo = vinfofile:read()
 local files = vinfo.files
+local info = 'data/versioninfo.kvp'
 
 vinfofile:close()
 
 if component.isAvailable('internet') then
-	local url = vinfo.updateurl[1]
-	shell.execute('wget -fq '..url..'data/versioninfo.kvp '..shell.getWorkingDirectory()..'/beamer/data/versioninfo.kvp')
+	print('Checking for updates')
+	local url = vinfo.updateurl[1]:match( "^%s*(.-)%s*$" )
+	local target = shell.getWorkingDirectory()..'/beamer/data/versioninfo.kvp'
+	shell.execute('wget -fq '..url..info..' '..target)
 	
 	local vinfofile = kvpreader:open(shell.getWorkingDirectory()..'/beamer/data/versioninfo')
 	local newvinfo = vinfofile:read()
 	
 	vinfofile:close()
 	
-	if newvinfo.version[1] ~= vinfo.version[1] then
-		print('Update found: '..vinfo.version[1]..' >> '..newvinfo.version[1])
+	if newvinfo.version[1]:match( "^%s*(.-)%s*$" ) ~= vinfo.version[1]:match( "^%s*(.-)%s*$" ) then
+		print('Update found: '..vinfo.version[1]:match( "^%s*(.-)%s*$" )..' >> '..newvinfo.version[1]:match( "^%s*(.-)%s*$" ))
+		print('Updating '..tostring(#files)..' files')
 		for _, file in ipairs(files) do
+			print('Downloading file: '..file)
 			shell.execute('wget -fq '..url..file..' '..shell.getWorkingDirectory()..'/beamer/'..file)
 		end
 		print('Update complete')
+	else
+		print('No new version to download')
 	end
+else
+	print('Failed to check for updates: No Internet Card Installed')
 end
 
+os.sleep(3)
+
 local vinf = kvpreader:open(shell.getWorkingDirectory()..'/beamer/data/versioninfo')
-local version = vinf:read().version[1]
+local version = vinf:read().version[1]:match( "^%s*(.-)%s*$" )
 vinf:close()
 return version
